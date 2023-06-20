f72befa140b31cdbf9a4fb5fc5cd50778279a964
Andy Berger <inocid@gmail.com>
2023-06-20 10:57:49 +0200

    Adding LittleLemonAnimation, an example of a user interface with a button and an animation

---
+++ LittleLemonAnimation/app/src/main/java/com/example/littlelemonanimation/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.littlelemonanimation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.littlelemonanimation.ui.theme.LittleLemonAnimationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonAnimationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var visible by remember {
+        mutableStateOf(true)
+    }
+    Column {
+        Button(onClick = { visible = !visible }) {
+            Text("Button")
+        }
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Text(text = "Hello, world!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LittleLemonAnimationTheme {
+        Greeting("Android")
+    }
+}
