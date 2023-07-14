47a636954871c7e21e0c9f4abdefb90dd0821d22
Andy Berger <inocid@gmail.com>
2023-07-14 10:27:02 +0200

    Adding LittleLemonReactNativeLoginPage, an example of using TextInput to build a login page

---
+++ LittleLemonReactNativeLoginPage/LoginScreen.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { ScrollView, Text, StyleSheet, TextInput } from 'react-native';
+
+export default function LoginScreen() {
+  const [email, onChangeEmail] = useState('');
+  const [password, onChangePassword] = useState('');
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+      <Text style={styles.regularText}>Login to continue </Text>
+      <TextInput
+        style={styles.inputBox}
+        value={email}
+        onChangeText={onChangeEmail}
+        placeholder={'email'}
+        keyboardType={'email-address'}
+      />
+      <TextInput
+        style={styles.inputBox}
+        value={password}
+        onChangeText={onChangePassword}
+        placeholder={'password'}
+        keyboardType={'default'}
+        secureTextEntry={true}
+      />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerText: {
+    padding: 40,
+    fontSize: 30,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  inputBox: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#EDEFEE',
+    backgroundColor: '#EDEFEE'
+  }
+});
