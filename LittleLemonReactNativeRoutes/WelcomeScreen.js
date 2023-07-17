bf8f4b5bf305300485d6947a0bc97499f157628b
Andy Berger <inocid@gmail.com>
2023-07-17 13:48:03 +0200

    Adding LittleLemonReactNativeRoutes, an example of setting up routes and setting a default route

---
+++ LittleLemonReactNativeRoutes/WelcomeScreen.js
@@ -0,0 +1,59 @@
+import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
+
+export default function WelcomeScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.headerWrapper}>
+        <Image
+          style={styles.image}
+          source={require('./img/logo.png')}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel={'Little Lemon Logo'}
+        />
+
+        <Text style={styles.headerText}>Little Lemon</Text>
+      </View>
+      <Text style={styles.regularText}>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear your experience with us!
+      </Text>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333333"
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  headerText: {
+    paddingRight: 10,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 10,
+    fontSize: 30,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+  },
+});
+
+
